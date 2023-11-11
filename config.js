17dcfcd89c77b4b7ce816202019f47c2bfbd7c13
Tharuka Randima <121140826+Xtharuka@users.noreply.github.com>
2023-11-11 07:19:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,21 +8,21 @@ global.zApiKey = {// if this API key get expired then please go to https://www.f
 }
 
 
-global.botname = process.env.BotName || "Queen Anya" 
-global.author = process.env.Author || "@PikaBotz" 
-global.packname = process.env.PackName || "Queen Anya v2 MD" 
+global.botname = process.env.BotName || "Xtharuka RANDIMA" 
+global.author = process.env.Author || "XTHARUKA" 
+global.packname = process.env.PackName || "RANDIMA" 
 global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© Queen Anya Bot" 
-global.prefa = process.env.Prefix || ['-'] 
+global.footer = process.env.Footer || "© POWERD BY XTHARUKA" 
+global.prefa = process.env.Prefix || ['.'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Pika~Kun" 
-global.ownernumber = process.env.Owner_Number || "916900904828" 
+global.ownername = process.env.Owner_Name || "XTHARUKA" 
+global.ownernumber = process.env.Owner_Number || "94718646926" 
 global.adress = process.env.Continent || "Asia, India, Assam" 
 global.timezone = process.env.TimeZone || "Asia/Kolkata" 
-global.instagramId = process.env.Insta || "8.08_only_mine" 
-global.email = process.env.Email_Id || "example@example.com" 
+global.instagramId = process.env.Insta || "tharuka_randima" 
+global.email = process.env.Email_Id || "tharukarandima@yahoo.com" 
 
-global.mongoUrl = process.env.MongoDB || "Enter"; 
+global.mongoUrl = process.env.MongoDB || "mongodb://un7jdhbyo5qxa7u7qtv6:YkA6D3aTGr6kDK3caSL@bfdtb5ntvp2zgde3u44m-mongodb.services.clever-cloud.com:2279/bfdtb5ntvp2zgde3u44m"; 
 
 global.warns = process.env.Warns_Limits || 3; 
 
@@ -101,21 +101,21 @@ global.message = {
 },
 
 module.exports = {
-  botname: process.env.BotName || "Queen Anya", 
-  author: process.env.Author || "@PikaBotz",
-  packname: process.env.PackName || "Queen Anya v2 MD",
+  botname: process.env.BotName || "Xtharuka RANDIMA", 
+  author: process.env.Author || "XTHARUKA",
+  packname: process.env.PackName || "RANDIMA",
   myweb: process.env.Web || "https://github.com/PikaBotz",
-  footer: process.env.Footer || "© Queen Anya Bot",
-  prefa: process.env.Prefix || ['-'],
+  footer: process.env.Footer || "© POWERD BY XTHARUKA",
+  prefa: process.env.Prefix || ['.'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Pika~Kun",
-  ownernumber: process.env.Owner_Number || "916900904828",
+  ownername: process.env.Owner_Name || "XTHARUKA",
+  ownernumber: process.env.Owner_Number || "94718646926",
   adress: process.env.Continent || "Asia, India, Assam",
   timezone: process.env.TimeZone || "Asia/Kolkata", 
-  instagramId: process.env.Insta || "8.08_only_mine",
-  email: process.env.Email_Id || "example@example.com",
+  instagramId: process.env.Insta || "tharuka_randima",
+  email: process.env.Email_Id || "tharukarandima@yahoo.com",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "Enter",
+  mongoUrl: process.env.MongoDB || "mongodb://un7jdhbyo5qxa7u7qtv6:YkA6D3aTGr6kDK3caSL@bfdtb5ntvp2zgde3u44m-mongodb.services.clever-cloud.com:2279/bfdtb5ntvp2zgde3u44m",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
